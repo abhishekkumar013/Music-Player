7f5f42c21a1562f50b53a77074b7b944f706fa4d
Abhishek kumar <sahaabhishek443@gmail.com>
2024-09-13 00:02:10 +0530

    first commit

---
+++ src/components/ControlCenter.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Pressable, StyleSheet, View} from 'react-native';
+import TrackPlayer, {State, usePlaybackState} from 'react-native-track-player';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export const ControlCenter = () => {
+  const playBackState = usePlaybackState();
+
+  // Next button
+  const skipToNext = async () => {
+    await TrackPlayer.skipToNext();
+  };
+
+  // Previous button
+  const skipToPrev = async () => {
+    await TrackPlayer.skipToPrevious();
+  };
+
+  const togglePlayback = async (playbackState: State) => {
+    const currentTrack = await TrackPlayer.getActiveTrack();
+    if (currentTrack !== null) {
+      if (playbackState === State.Paused || playbackState === State.Ready) {
+        await TrackPlayer.play();
+      } else {
+        await TrackPlayer.pause();
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={skipToPrev}>
+        <Icon style={styles.icon} name="skip-previous" size={40} />
+      </Pressable>
+      <Pressable onPress={() => togglePlayback(playBackState)}>
+        <Icon
+          style={styles.icon}
+          name={playBackState === State.Playing ? 'pause' : 'play-arrow'}
+          size={75}
+        />
+      </Pressable>
+      <Pressable onPress={skipToNext}>
+        <Icon style={styles.icon} name="skip-next" size={40} />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 56,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    color: '#FFFFFF',
+  },
+  playButton: {
+    marginHorizontal: 24,
+  },
+});
+
+export default ControlCenter;
